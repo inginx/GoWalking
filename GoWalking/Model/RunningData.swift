7b7df5e2505cee731a85666d5f4364660881232f
称一称 <496342862@qq.com>
2015-11-28 19:18:05 +0800

    历史记录

---
+++ GoWalking/Model/RunningData.swift
@@ -0,0 +1,13 @@
+//
+//  RunningData.swift
+//  GoWalking
+//
+//  Created by 称一称 on 15/11/27.
+//  Copyright © 2015年 称一称. All rights reserved.
+//
+
+import Cocoa
+
+class RunningData: NSObject {
+
+}
---
+++ GoWalking/historyViewController.swift
@@ -0,0 +1,13 @@
+//
+//  historyViewController.swift
+//  GoWalking
+//
+//  Created by 称一称 on 15/11/27.
+//  Copyright © 2015年 称一称. All rights reserved.
+//
+
+import Cocoa
+
+class historyViewController: UIViewController {
+
+}
